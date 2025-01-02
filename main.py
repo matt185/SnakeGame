6cb65dc883be05d3e24de93ec04c3dab8d1cedca
Mattia <59755795+matt185@users.noreply.github.com>
2025-01-02 13:28:58 +0900

    implement snake logic

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+import time
 from turtle import Screen
+from components.snake import Snake
 
 # create the game board
 screen = Screen()
@@ -7,8 +9,24 @@ screen.bgcolor("black")
 screen.title("Snake Game")
 screen.tracer(0)
 
+# create game element
+snake = Snake()
+
+# set the snake movement
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.right, "Right")
+screen.onkey(snake.left, "Left")
+
 game_over = False
 while not game_over:
     screen.update()
+    time.sleep(0.1)
+    snake.move()
 
+    # detect the collision with the wall
+    if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
+        snake.reset()
+        
 screen.exitonclick()
